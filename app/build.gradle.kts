d600659205ab952f7b0e806e360ef03ac3d23b99
BuddaSet <buddaset@mail.ru>
2022-08-11 12:08:48 +0300

    Update Grable dependencies and buildSrc

---
+++ app/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdkVersion(ConfigVersions.compileSdkVersion)
+
+    defaultConfig {
+        applicationId = ConfigVersions.applicationId
+        minSdk = ConfigVersions.minSdkVersion
+        targetSdk = ConfigVersions.targetSdkVersion
+        versionCode = ConfigVersions.versionCode
+        versionName = ConfigVersions.versionName
+
+        testInstrumentationRunner ="androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = ConfigVersions.javaVersion
+        targetCompatibility = ConfigVersions.javaVersion
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(project(Modules.core))
+
+    implementation(Dependencies.Androidx.coreKtx)
+    implementation(Dependencies.Androidx.appCompat)
+    implementation(Dependencies.UI.constraintLayout)
+    implementation(Dependencies.UI.materialComponents)
+
+
+    implementation(Dependencies.Navigation.navigationComponentFragmentKtx)
+    implementation(Dependencies.Navigation.navigationComponentUiKtx)
+
+    testImplementation(Dependencies.Tests.junit)
+    androidTestImplementation(Dependencies.Tests.junitExt)
+    androidTestImplementation(Dependencies.Tests.junitExt)
+
+
+}
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,4 +1,71 @@
+import org.gradle.api.JavaVersion
+
 object Versions {
+
+    // UI
+    const val materialComponent = "1.6.1"
+    const val constraintLayout = "2.1.4"
+    const val appCompat = "1.4.2"
+
+    // Kotlin
+    const val coreKtx = "1.7.0"
+
+    // Navigation
+    const val navigationComponent = "2.5.1"
+
+    //Test
+    const val junit = "4.13.2"
+    const val junitExt = "1.1.3"
+    const val espresso = "3.4.0"
+
+
+}
+
+object Dependencies {
+
+    object Androidx {
+        const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+        const val coreKtx = "androidx.core:core-ktx:${Versions.coreKtx}"
+    }
+
+    object UI {
+        const val materialComponents =
+            "com.google.android.material:material:${Versions.materialComponent}"
+        const val constraintLayout =
+            "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+
+    }
+
+    object Navigation {
+        const val navigationComponentFragmentKtx =
+            "androidx.navigation:navigation-fragment-ktx:${Versions.navigationComponent}"
+        const val navigationComponentUiKtx =
+            "androidx.navigation:navigation-ui-ktx:${Versions.navigationComponent}"
+
+    }
+
+    object Tests {
+        const val junit = "junit:junit:${Versions.junit}"
+        const val junitExt = "androidx.test.ext:junit:${Versions.junitExt}"
+        const val espresso = "androidx.test.espresso:espresso-core:${Versions.espresso}"
+
+    }
+
+
+}
+
+
+object Modules {
+
+    const val app = ":app"
+    const val core = ":core"
+    const val mainScreen = ":feature-main-screen"
+
+
+}
+
+
+object ConfigVersions {
 
     const val compileSdkVersion = 32
     const val targetSdkVersion = 32
@@ -7,52 +74,9 @@ object Versions {
     const val versionCode = 1
     const val versionName = "1.0"
     const val applicationId = "com.example.markettesttask"
+    const val kotlinVersion = "1.6.20"
+    val javaVersion = JavaVersion.VERSION_1_8
 
-    const val kotlin = "1.6.20"
-}
-
-object Dependencies {
-
-    val ui = UI
-    val androidX = AndroidX
-    val jetpack = Jetpack
-    val test = Test
-
-    object UI {
-        private const val materialComponentVersion = "1.6.1"
-
-        const val materialComponents =
-            "com.google.android.material:material:$materialComponentVersion"
-    }
-
-    object AndroidX {
-        private const val constraintLayoutVersion = "2.1.4"
-        private const val appCompatVersion = "1.4.2"
-        private const val coreKtxVersion = "1.7.0"
-
-        const val constraintLayout =
-            "androidx.constraintlayout:constraintlayout:$constraintLayoutVersion"
-        const val appCompat = "androidx.appcompat:appcompat:$appCompatVersion"
-        const val coreKtx = "androidx.core:core-ktx:$coreKtxVersion"
-    }
-
-    object Jetpack {
-        private const val navigationComponentVersion = "2.5.1"
-
-        const val navigationComponentFragmentKtx = "androidx.navigation:navigation-fragment-ktx:$navigationComponentVersion"
-        const val navigationComponentUiKtx = "androidx.navigation:navigation-ui-ktx:$navigationComponentVersion"
-    }
-
-
-    object Test {
-        private const val junitVersion = "4.13.2"
-        private const val junitExtVersion = "1.1.3"
-        private const val espressoVersion = "3.4.0"
-
-        const val junit = "junit:junit:$junitVersion"
-        const val junitExt = "androidx.test.ext:junit:$junitExtVersion"
-        const val espresso = "androidx.test.espresso:espresso-core:$espressoVersion"
-    }
 }
 
 
