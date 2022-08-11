3e54e004516b6d3ed84934048ed9d30862c1b86f
BuddaSet <buddaset@mail.ru>
2022-08-11 13:24:09 +0300

    Create bottom navigation and connecting it to main screen graph (feature-main-screen)

--- app/src/main/java/com/example/markettesttask/MainActivity.kt
+++ app/src/main/java/com/example/markettesttask/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.markettesttask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.markettesttask.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
---
+++ feature-main-screen/src/main/java/com/example/feature_main_screen/presentation/MainFragment.kt
@@ -0,0 +1,27 @@
+package com.example.feature_main_screen.presentation
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.feature_main_screen.R
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
+
+
+}
