4678f1cbdd9f86616d1fd77b8b869a266009c12c
BuddaSet <buddaset@mail.ru>
2022-08-14 13:30:37 +0300

    Create common UiState sealed class

--- core/src/main/java/com/example/core/precentation/UiState.kt
+++ core/src/main/java/com/example/core/precentation/UiState.kt
@@ -2,9 +2,9 @@ package com.example.core.precentation
 
 sealed class UiState<out T> {
 
-    class Loading<T>() : UiState<T>()
+    class Loading<T> : UiState<T>()
 
-    class Error<T>(error: Throwable) : UiState<T>()
+    class Error<T>(val error: Throwable) : UiState<T>()
 
     class Success<T>(val data: T) : UiState<T>()
 
