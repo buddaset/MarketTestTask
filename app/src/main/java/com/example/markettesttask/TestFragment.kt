7c903d575d2b247256191dae789c704bdaf40e85
BuddaSet <buddaset@mail.ru>
2022-08-15 17:05:47 +0300

    Add test fragment

---
+++ app/src/main/java/com/example/markettesttask/TestFragment.kt
@@ -0,0 +1,16 @@
+package com.example.markettesttask
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+class TestFragment : Fragment(R.layout.fragment_test)
+
+
+
+
+
