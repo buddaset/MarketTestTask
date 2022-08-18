aa0d0faffdfb23a454d2f7a90c25be8c5891b8d1
BuddaSet <buddaset@mail.ru>
2022-08-18 18:53:07 +0300

    Create modules feature-product-details and core-ui

---
+++ feature-product-details/build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id ("kotlinx-serialization")
+    id ("kotlin-kapt")
+}
+
+android {
+    compileSdk = ConfigVersions.compileSdkVersion
+
+    defaultConfig {
+        minSdk = ConfigVersions.minSdkVersion
+        targetSdk = ConfigVersions.targetSdkVersion
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+
+        }
+    }
+    buildFeatures.viewBinding = true
+}
+dependencies {
+
+
+    implementation(project(Modules.core))
+
+    implementation(Dependencies.AndroidKTX.core)
+    implementation(Dependencies.AndroidKTX.runtime)
+    implementation(Dependencies.AndroidKTX.viewModel)
+    implementation(Dependencies.AndroidKTX.fragment)
+
+    implementation(Dependencies.UI.appCompat)
+    implementation(Dependencies.UI.constraintLayout)
+    implementation(Dependencies.UI.materialComponents)
+
+    implementation(Dependencies.Navigation.navigationComponentFragmentKtx)
+    implementation(Dependencies.Navigation.navigationComponentUiKtx)
+
+    implementation(Dependencies.Dagger2.dagger2)
+    kapt(Dependencies.Dagger2.kapt)
+
+
+    // Retrofit
+    implementation(Dependencies.Retrofit.retrofit)
+    implementation(Dependencies.Retrofit.jsonSerialization)
+    implementation(Dependencies.Retrofit.converter)
+    implementation(Dependencies.Retrofit.interceptor)
+
+
+    // Coroutines
+    implementation(Dependencies.Kotlin.coroutinesCore)
+    implementation(Dependencies.Kotlin.coroutinesAndroid)
+
+    implementation(Dependencies.AdapterDelegates.adapterDelegates)
+    implementation(Dependencies.AdapterDelegates.viewBinding)
+
+    implementation(Dependencies.Glide.glide)
+    kapt(Dependencies.Glide.kapt)
+
+
+
+}
