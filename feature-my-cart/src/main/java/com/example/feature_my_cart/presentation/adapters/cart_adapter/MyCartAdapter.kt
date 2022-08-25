affa036195200b897803431b1d4ab1cdc2ca002c
BuddaSet <buddaset@mail.ru>
2022-08-25 22:23:34 +0300

    Create adapter for my cart items

---
+++ feature-my-cart/src/main/java/com/example/feature_my_cart/presentation/adapters/cart_adapter/MyCartAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.feature_my_cart.presentation.adapters.cart_adapter
+
+import androidx.recyclerview.widget.DiffUtil
+import com.bumptech.glide.Glide
+import com.example.feature_my_cart.databinding.MyCartItemBinding
+import com.example.feature_my_cart.domain.model.Basket
+import com.hannesdorfmann.adapterdelegates4.AsyncListDifferDelegationAdapter
+import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegateViewBinding
+
+class MyCartAdapter : AsyncListDifferDelegationAdapter<Basket>(DIFF_UTIL_CALLBACK) {
+
+
+    init {
+        delegatesManager
+            .addDelegate(myCartAdapterDelegate())
+    }
+
+
+    private fun myCartAdapterDelegate() =
+        adapterDelegateViewBinding<Basket, Basket, MyCartItemBinding>(
+            { inflater, parent -> MyCartItemBinding.inflate(inflater, parent, false) }
+        ) {
+            bind {
+                binding.nameItemTextView.text = item.title
+                binding.priceItemTextView.text = item.price.toString()
+                Glide.with(itemView)
+                    .load(item.images)
+                    .into(binding.cartItemImageView)
+            }
+        }
+
+
+    companion object {
+        private val DIFF_UTIL_CALLBACK = object : DiffUtil.ItemCallback<Basket>() {
+            override fun areItemsTheSame(oldItem: Basket, newItem: Basket): Boolean =
+                oldItem.id == newItem.id
+
+
+            override fun areContentsTheSame(oldItem: Basket, newItem: Basket): Boolean =
+                oldItem == newItem
+
+
+        }
+    }
+}
