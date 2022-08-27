8a1c793d818780714bdc09030d51ad6aeeb704fa
BuddaSet <buddaset@mail.ru>
2022-08-27 16:54:15 +0300

    Add dependencies

--- feature-splash-screen/build.gradle.kts
+++ feature-splash-screen/build.gradle.kts
@@ -1,43 +1,46 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+
 }
 
 android {
-    compileSdk = 32
+    compileSdk = ConfigVersions.compileSdkVersion
 
     defaultConfig {
-        minSdk = 23
-        targetSdk = 32
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
+        minSdk = ConfigVersions.minSdkVersion
+        targetSdk = ConfigVersions.targetSdkVersion
     }
-
     buildTypes {
         release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            isMinifyEnabled = true
+
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+    buildFeatures.viewBinding = true
 }
-
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.5.0")
-    implementation("com.google.android.material:material:1.6.1")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+
+
+    implementation(project(Modules.core_ui))
+
+
+    implementation(Dependencies.AndroidKTX.core)
+    implementation(Dependencies.AndroidKTX.runtime)
+    implementation(Dependencies.AndroidKTX.fragment)
+
+    implementation(Dependencies.UI.appCompat)
+    implementation(Dependencies.UI.constraintLayout)
+    implementation(Dependencies.UI.materialComponents)
+
+    implementation(Dependencies.Navigation.navigationComponentFragmentKtx)
+    implementation(Dependencies.Navigation.navigationComponentUiKtx)
+
+
+
+
+
+
+
 }
