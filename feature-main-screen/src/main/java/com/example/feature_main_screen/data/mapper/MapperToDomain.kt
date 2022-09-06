773d7fc211eaaa688222bc4d6ad19a7abe14a604
BuddaSet <buddaset@mail.ru>
2022-09-07 01:41:00 +0300

    Create additional mappers for main screen data

--- feature-main-screen/src/main/java/com/example/feature_main_screen/data/mapper/MapperToDomain.kt
+++ feature-main-screen/src/main/java/com/example/feature_main_screen/data/mapper/MapperToDomain.kt
@@ -1,5 +1,8 @@
 package com.example.feature_main_screen.data.mapper
 
+import com.example.feature_main_screen.data.local.model.BestSellerEntity
+import com.example.feature_main_screen.data.local.model.HotSalesEntity
+import com.example.feature_main_screen.data.local.model.MainScreenDataEntityContainer
 import com.example.feature_main_screen.data.remote.model.BestSellerDto
 import com.example.feature_main_screen.data.remote.model.HotSalesDto
 import com.example.feature_main_screen.data.remote.model.MainScreenDataDto
@@ -42,5 +45,47 @@ internal fun HotSalesDto.toDomain(): HotSales =
 @JvmName("toDomainHomeStoreDto")
 internal fun List<HotSalesDto>.toDomain(): List<HotSales> =
     this.map { HomeStoreDto -> HomeStoreDto.toDomain() }
+
+
+
+
+
+internal fun MainScreenDataEntityContainer.toDomain() =
+    MainScreenData(
+        bestSeller = bestSeller.toDomain(),
+        hotSales =  hotSales.toDomain()
+    )
+
+
+internal fun BestSellerEntity.toDomain() =
+    BestSeller(
+        discountPrice = discountPrice,
+        id = id,
+        isFavorites = isFavorites,
+        picture = picture,
+        priceWithoutDiscount = priceWithoutDiscount,
+        title = title
+    )
+
+@JvmName("toDomainBestSellerEntity")
+internal fun List<BestSellerEntity>.toDomain(): List<BestSeller> =
+    this.map { bestSellerEntity -> bestSellerEntity.toDomain() }
+
+
+internal fun HotSalesEntity.toDomain(): HotSales =
+    HotSales(
+        id = id,
+        isBuy = isBuy,
+        isNew = isNew,
+        picture = picture,
+        subtitle = subtitle,
+        title = title
+    )
+
+
+@JvmName("toDomainHotSalesEntity")
+internal fun List<HotSalesEntity>.toDomain(): List<HotSales> =
+    this.map { HomeStoreEntity -> HomeStoreEntity.toDomain() }
+
 
 
