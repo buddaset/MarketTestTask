f72b06b19fd9ff0b1b25784136098b3e1713cb9c
BuddaSet <buddaset@mail.ru>
2022-09-07 21:30:14 +0300

    Update data classes for my-cart (room)

--- feature-my-cart/src/main/java/com/example/feature_my_cart/data/local/model/CartBasketCrossRefEntity.kt
+++ feature-my-cart/src/main/java/com/example/feature_my_cart/data/local/model/CartBasketCrossRefEntity.kt
@@ -5,8 +5,8 @@ import androidx.room.Entity
 
 
 @Entity(tableName = "carts_baskets_cross_ref",
-primaryKeys = ["card_id", "basket_id"])
+primaryKeys = ["cart_id", "basket_id"])
 data class CartBasketCrossRefEntity(
     @ColumnInfo(name="cart_id") val  cartId: String,
-    @ColumnInfo(name="basket_id")val basketId: String
+    @ColumnInfo(name="basket_id")val basketId: Int
 )
--- feature-my-cart/src/main/java/com/example/feature_my_cart/data/local/model/CartEntity.kt
+++ feature-my-cart/src/main/java/com/example/feature_my_cart/data/local/model/CartEntity.kt
@@ -11,4 +11,7 @@ data class CartEntity(
     @Ignore val basket: List<BasketEntity>,
     @ColumnInfo(name = "delivery") val delivery: String,
     @ColumnInfo(name = "total") val total: Int
-)
+){
+
+    constructor(id: String, delivery: String, total: Int) : this(id, emptyList(), delivery, total)
+}
